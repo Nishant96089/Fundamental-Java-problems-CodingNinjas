3a4104a8be76436bb817626a25e3aa6c5cda49ea
Nishant Sinha <126960153+Nishant96089@users.noreply.github.com>
2023-05-01 21:05:40 +0530

    Update and rename Fahrenheit to Celsius Table to Fahrenheit to Celsius Table.java

--- Conditionals and Loops/Fahrenheit to Celsius Table.java
+++ Conditionals and Loops/Fahrenheit to Celsius Table.java
@@ -1,3 +1,4 @@
+/*
 Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E) and Step Size (W), you need to convert all Fahrenheit values from Start to End at the gap of W, into their corresponding Celsius values and print the table.
 Input Format :
 3 integers - S, E and W respectively 
@@ -33,7 +34,7 @@ Sample Output 2:
 111 43
 Explanation For Input 2:
 Start calculating the Celsius values for each Fahrenheit Value which starts from 20. So starting from 20, we need to compute its corresponding Celsius value which computes to -6. We print this information as <Fahrenheit Value> <a single space> <Celsius Value> on each line. Now add 13 to Fahrenheit Value at each step until you reach 119 in this case. You may or may not exactly land on the end value depending on the steps you are taking.
-
+*/
 
 
 
